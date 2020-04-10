f973b9b1770c48086dc088e937c4df552e4ec956
Ilmam Fauzi <ilmam.javajin@gmail.com>
2020-04-10 21:01:26 +0700

    Fitur peta dengan data kasus positif, msembuh & meninggal

--- map.js
+++ map.js
@@ -1,8 +1,6 @@
 const mymap = L.map('mapid').setView([0, 0], 2);
 
-// const marker = L.marker([0, 0], {
-//   draggable: true
-// }).addTo(mymap);
+// const marker = L.marker([0, 0], {}).addTo(mymap);
 
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>';
 const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
@@ -11,8 +9,37 @@ const tiles = L.tileLayer(tileUrl, {
 });
 tiles.addTo(mymap);
 
-const lat = 50.5;
-const lng = 30.5;
+$.ajax({
+  url: 'https://api.kawalcorona.com/',
+  type: 'GET',
+  async: true,
+  dataType: 'json',
+  success: function (response) {
+    $.each(response, function (i, data) {
+      // console.log(response[i].attributes.Country_Region);
+      const negara = response[i].attributes.Country_Region;
+      const positif = response[i].attributes.Confirmed;
+      const sembuh = response[i].attributes.Recovered;
+      const meninggal = response[i].attributes.Deaths;
+      const lat = response[i].attributes.Lat;
+      const lng = response[i].attributes.Long_;
+      const marker = L.marker([lat, lng], {}).addTo(mymap);
+
+      marker.bindPopup(`<b>` + negara + `<br />
+      Positif : </b>` + positif + `<br />
+      <b>Sembuh : </b>` + sembuh + `<br />
+      <b>Meninggal : </b>` + meninggal).openPopup();
+    });
+  }
+});
+
+$(document).ready(function () {
+
+});
+
+//marker
+// const lat = 50.5;
+// const lng = 30.5;
 // marker.setLatLng([lat, lng]);
 // mymap.setView([lat, lng], 12);
 
