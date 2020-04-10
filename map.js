b0e6fb705a5d60f338c8be4b4cd289e5696e3ff8
Ilmam Fauzi <ilmam.javajin@gmail.com>
2020-04-10 23:43:15 +0700

    Map bug fixing

--- map.js
+++ map.js
@@ -1,55 +1,40 @@
-const mymap = L.map('mapid').setView([0, 0], 2);
-
-// const marker = L.marker([0, 0], {}).addTo(mymap);
-
-const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>';
-const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-const tiles = L.tileLayer(tileUrl, {
-  attribution
-});
-tiles.addTo(mymap);
-
-$.ajax({
-  url: 'https://api.kawalcorona.com/',
-  type: 'GET',
-  async: true,
-  dataType: 'json',
-  success: function (response) {
-    $.each(response, function (i, data) {
-      // console.log(response[i].attributes.Country_Region);
-      const negara = response[i].attributes.Country_Region;
-      const positif = response[i].attributes.Confirmed;
-      const sembuh = response[i].attributes.Recovered;
-      const meninggal = response[i].attributes.Deaths;
-      const lat = response[i].attributes.Lat;
-      const lng = response[i].attributes.Long_;
-      const marker = L.marker([lat, lng], {}).addTo(mymap);
-
-      marker.bindPopup(`<b>` + negara + `<br />
-      Positif : </b>` + positif + `<br />
-      <b>Sembuh : </b>` + sembuh + `<br />
-      <b>Meninggal : </b>` + meninggal).openPopup();
-    });
-  }
-});
-
 $(document).ready(function () {
+  const mymap = L.map('mapid').setView([0, 0], 2);
 
+  const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+  const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+  const tiles = L.tileLayer(tileUrl, {
+    attribution
+  });
+  tiles.addTo(mymap);
+
+  $.ajax({
+    url: 'https://api.kawalcorona.com/',
+    type: 'GET',
+    async: true,
+    dataType: 'json',
+    success: function (response) {
+      $.each(response, function (i, data) {
+        const negara = response[i].attributes.Country_Region;
+        const positif = response[i].attributes.Confirmed;
+        const sembuh = response[i].attributes.Recovered;
+        const meninggal = response[i].attributes.Deaths;
+        const lat = response[i].attributes.Lat;
+        const lng = response[i].attributes.Long_;
+        const marker = L.marker([lat, lng], {}).addTo(mymap);
+        marker.bindPopup().openPopup();
+
+        function showPopup() {
+          this.bindPopup(
+            `<b>` + negara + `<br />
+            Positif : </b>` + positif + `<br />
+            <b>Sembuh : </b>` + sembuh + `<br />
+            <b>Meninggal : </b>` + meninggal
+          ).openPopup();
+        }
+
+        $(marker).click(showPopup);
+      });
+    }
+  });
 });
-
-//marker
-// const lat = 50.5;
-// const lng = 30.5;
-// marker.setLatLng([lat, lng]);
-// mymap.setView([lat, lng], 12);
-
-// function putMarker() {
-//   marker.on('dragend', function(e) {
-//     $("#lat").val(this.getLatLng().lat);
-//     $("#lng").val(this.getLatLng().lng);
-//   });
-// }
-
-// $(document).ready(function() {
-//   putMarker();
-// });
